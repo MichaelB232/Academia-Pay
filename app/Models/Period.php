b01c60e6bce6397f0a7d3a0ff4c67e4a13355877
MichaelB232 <michaelbintang2006@gmail.com>
2026-06-08 22:17:29 +0700

    finishing period model

--- app/Models/Period.php
+++ app/Models/Period.php
@@ -6,6 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Period extends Model
 {
-    //
     protected $fillable = ['bulan', 'tahun', 'status'];
+
+    public function payrolls()
+    {
+        return $this->hasMany(Payroll::class);
+    }
+
+    public function disciplineAssessments()
+    {
+        return $this->hasMany(DisciplineAssessment::class);
+    }
+
+    public function kpiAssessments()
+    {
+        return $this->hasMany(KpiAssessment::class);
+    }
 }
